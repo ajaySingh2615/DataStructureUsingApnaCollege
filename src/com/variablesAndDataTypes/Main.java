9b3a49d404653e54ae10f8ea978d713bb4b9687c
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-20 16:54:39 +0530

    VariablesAndDataTypes Done

---
+++ src/com/variablesAndDataTypes/Main.java
@@ -0,0 +1,25 @@
+package com.variablesAndDataTypes;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // taking output
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a = sc.nextInt();
+        System.out.println("Enter b:");
+        int b = sc.nextInt();
+
+//        System.out.println("sum is :" + (a+b));
+
+        // product of a and b
+
+        System.out.println("product of two numbers: " + (a*b));
+
+        // Area of a circle
+        int $ = 10;
+    }
+}
