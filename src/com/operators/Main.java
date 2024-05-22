1fc74f5ce686e08d61f97b940708a3ef4e1463e1
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-22 12:32:26 +0530

    "Operators done"

---
+++ src/com/operators/Main.java
@@ -0,0 +1,28 @@
+package com.operators;
+
+public class Main {
+    public static void main(String[] args) {
+        // Symbols that tell compiler to perform some operators
+        // sum = a + b
+
+        // Arithmetic Operator
+        // Relational Operator
+        // Logical Operator
+        // Assignment Operator
+
+
+        // Unary Operator
+        // ++
+        // Pre Increment ++a
+        int a = 10;
+        int b = ++a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Post Increment a++
+        int aa = 10;
+        int bb = aa++;
+        System.out.println("aa = " + aa);
+        System.out.println("bb = " + bb);
+    }
+}
